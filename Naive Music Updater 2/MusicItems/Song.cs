a0f544b020fb56cbe1510eacab3d43170365bfd5
tryashtar <tryashtar@gmail.com>
2022-09-13 00:42:11 -0600

    Check ID3V2 for replaygain

--- Naive Music Updater 2/MusicItems/Song.cs
+++ Naive Music Updater 2/MusicItems/Song.cs
@@ -26,8 +26,8 @@ public class Song : IMusicItem
     {
         Logger.WriteLine($"Song: {SimpleName}", ConsoleColor.Gray);
 #if !DEBUG
-            if (!GlobalCache.NeedsUpdate(this))
-                return;
+        if (!GlobalCache.NeedsUpdate(this))
+            return;
 #endif
 #if DEBUG
         if (Breakpoint != null && !SimpleName.ToLower().Contains(Breakpoint) && !String.Join('/', PathFromRoot().Select(x => x.SimpleName)).ToLower().Contains(Breakpoint))
@@ -36,18 +36,18 @@ public class Song : IMusicItem
         Logger.WriteLine($"(checking)");
         var metadata = MusicItemUtils.GetMetadata(this, MetadataField.All);
 #if !DEBUG
-            bool reload_file = true;
-            using var replay_file = TagLib.File.Create(Location);
-            bool needs_replaygain = !HasReplayGain(replay_file);
-            if (needs_replaygain)
-            {
-                Logger.WriteLine($"Normalizing audio with ReplayGain");
-                GlobalCache.Config.NormalizeAudio(this);
-                replay_file.Dispose();
-            }
-            else
-                reload_file = false;
-            using var file = reload_file ? TagLib.File.Create(Location) : replay_file;
+        bool reload_file = true;
+        using var replay_file = TagLib.File.Create(Location);
+        bool needs_replaygain = !HasReplayGain(replay_file);
+        if (needs_replaygain)
+        {
+            Logger.WriteLine($"Normalizing audio with ReplayGain");
+            GlobalCache.Config.NormalizeAudio(this);
+            replay_file.Dispose();
+        }
+        else
+            reload_file = false;
+        using var file = reload_file ? TagLib.File.Create(Location) : replay_file;
 #else
         using var file = TagLib.File.Create(Location);
 #endif
@@ -60,20 +60,20 @@ public class Song : IMusicItem
         modifier.WriteChapters(path);
 
 #if !DEBUG
-            bool success = true;
-            if (modifier.HasChanged)
+        bool success = true;
+        if (modifier.HasChanged)
+        {
+            Logger.WriteLine("Saving...", ConsoleColor.Green);
+            try { file.Save(); }
+            catch (IOException ex)
             {
-                Logger.WriteLine("Saving...", ConsoleColor.Green);
-                try { file.Save(); }
-                catch (IOException ex)
-                {
-                    Logger.WriteLine($"Save failed because {ex.Message}! Skipping...", ConsoleColor.Red);
-                    GlobalCache.MarkNeedsUpdateNextTime(this);
-                    success = false;
-                }
+                Logger.WriteLine($"Save failed because {ex.Message}! Skipping...", ConsoleColor.Red);
+                GlobalCache.MarkNeedsUpdateNextTime(this);
+                success = false;
             }
-            if (success)
-                GlobalCache.MarkUpdatedRecently(this);
+        }
+        if (success)
+            GlobalCache.MarkUpdatedRecently(this);
 #else
         if (modifier.HasChanged)
             Logger.WriteLine("Changed!");
@@ -83,18 +83,25 @@ public class Song : IMusicItem
     private bool HasReplayGain(TagLib.File file)
     {
         const string TRACK_GAIN = "REPLAYGAIN_TRACK_GAIN";
+        var id3v2 = (TagLib.Id3v2.Tag)file.GetTag(TagTypes.Id3v2);
+        if (id3v2 != null)
+        {
+            var frames = id3v2.GetFrames<RelativeVolumeFrame>();
+            if (frames.Any())
+                return true;
+        }
         var ape = (TagLib.Ape.Tag)file.GetTag(TagTypes.Ape);
         if (ape != null)
-            return ape.HasItem(TRACK_GAIN);
-        else
         {
-            var ogg = (TagLib.Ogg.XiphComment)file.GetTag(TagTypes.Xiph);
-            if (ogg != null)
-            {
-                var gain = ogg.GetFirstField(TRACK_GAIN);
-                if (gain != null)
-                    return true;
-            }
+            if (ape.HasItem(TRACK_GAIN))
+                return true;
+        }
+        var ogg = (TagLib.Ogg.XiphComment)file.GetTag(TagTypes.Xiph);
+        if (ogg != null)
+        {
+            var gain = ogg.GetFirstField(TRACK_GAIN);
+            if (gain != null)
+                return true;
         }
         return false;
     }
