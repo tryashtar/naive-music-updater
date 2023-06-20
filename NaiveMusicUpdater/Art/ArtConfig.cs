99d4d43b6540ab72ddfc63331af5d6fbbc6302fc
tryashtar <tryashtar@gmail.com>
2023-06-20 03:42:26 -0600

    Allow lists in art config

--- NaiveMusicUpdater/Art/ArtConfig.cs
+++ NaiveMusicUpdater/Art/ArtConfig.cs
@@ -10,35 +10,48 @@ public class ArtConfig
         Owner = owner;
         var node = (YamlMappingNode)YamlHelper.ParseFile(Path.Combine(folder, relative, "images.yaml"))!;
         Settings = new();
-        
+
+        void add_all(Predicate<string> pred, IEnumerable<ProcessArtSettings> settings)
+        {
+            foreach (var set in settings)
+            {
+                Settings.Add((pred, set));
+            }
+        }
+
         var all = node.Go("all").NullableParse(LiteralOrReference);
         if (all != null)
-            Settings.Add((_ => true, all));
-        
+            add_all(_ => true, all);
+
         if (node.Children.TryGetValue("set all", out var set_all))
         {
             foreach (var item in (YamlSequenceNode)set_all)
             {
                 var names = item.Go("names").ToStringList()!.Select(x => Path.Combine(relative, x)).ToList();
                 var set = LiteralOrReference(item["set"]);
-                Settings.Add((x => names.Contains(x), set));
+                add_all(x => names.Contains(x), set);
             }
         }
-        
+
         var set_dict = node.Go("set").ToDictionary(x => x.String()!, LiteralOrReference);
         if (set_dict != null)
         {
             foreach (var (name, settings) in set_dict)
             {
-                Settings.Add((x => x == Path.Combine(relative, name), settings));
+                add_all(x => x == Path.Combine(relative, name), settings);
             }
         }
     }
 
-    private ProcessArtSettings LiteralOrReference(YamlNode node)
+    private IEnumerable<ProcessArtSettings> LiteralOrReference(YamlNode node)
     {
-        if (node is YamlScalarNode { Value: not null } scalar)
-            return Owner.NamedSettings[scalar.Value];
-        return new ProcessArtSettings((YamlMappingNode)node);
+        IEnumerable<YamlNode> list = node is YamlSequenceNode seq ? seq.Children : new[] { node };
+        foreach (var child in list)
+        {
+            if (child is YamlScalarNode { Value: not null } scalar)
+                yield return Owner.NamedSettings[scalar.Value];
+            if (child is YamlMappingNode map)
+                yield return new ProcessArtSettings(map);
+        }
     }
 }
